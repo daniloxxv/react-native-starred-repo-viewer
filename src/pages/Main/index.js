6e10ba318cafb651fd36eb42a0432407c141801c
daniloxxv <danilosoares@gmail.com>
2019-11-04 22:41:37 +0000

    Connecting to API

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Text, Keyboard} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
+
 import {Container, Form, Input, SubmitButton} from './styles';
 
 export default function Main() {
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState('');
+
+  const handleAddUser = async () => {
+    const response = await api.get(`/users/${newUser}`);
+    const {name, login, bio, avatar_url} = response.data;
+    setUsers([...users, {name, login, bio, avatar_url}]);
+    setNewUser('');
+    Keyboard.dismiss();
+  };
   return (
     <Container>
       <Form>
@@ -10,11 +23,18 @@ export default function Main() {
           autoCorrect={false}
           autoCapitalize="none"
           placeholder="Add user"
+          value={newUser}
+          onChangeText={text => setNewUser(text)}
+          returnKeyType="send"
+          onSubmitEditing={handleAddUser}
         />
-        <SubmitButton>
+        <SubmitButton onPress={handleAddUser}>
           <Icon name="add" size={20} color="#FFF" />
         </SubmitButton>
       </Form>
+      {users.map(user => (
+        <Text>{user.login}</Text>
+      ))}
     </Container>
   );
 }
