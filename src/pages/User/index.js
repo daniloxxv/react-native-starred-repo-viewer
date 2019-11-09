db458647361c9e3f5a2bbee96cd567d55f6bc215
daniloxxv <danilosoares@gmail.com>
2019-11-09 08:52:48 +0000

    Connecting to API on user page

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
 export default function User({navigation}) {
-  return <Text>{JSON.stringify(navigation.getParam('user'))}</Text>;
+  const [stars, setStars] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const user = navigation.getParam('user');
+      const response = await api.get(`/users/${user.login}/starred`);
+      setStars(response.data);
+    })();
+  }, [navigation]);
+
+  return <Text>{JSON.stringify(stars)}</Text>;
 }
+
+User.navigationOptions = ({navigation}) => ({
+  title: navigation.getParam('user').name,
+});
+
+User.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,,
+  }).isRequired,
+};
